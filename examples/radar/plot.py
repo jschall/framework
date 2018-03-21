ecbb9b0d701461449e8c275f0298d522a6ec38f6
Jonathan Challinger <mr.challinger@gmail.com>
2018-03-20 20:31:26 -0700

    wip

--- examples/radar/plot.py
+++ examples/radar/plot.py
@@ -9,19 +9,30 @@ node = uavcan.make_node(sys.argv[1])
 plt.ion()
 dist = [i*0.15 for i in range(60)]
 
+def wrap_pi(x):
+    if x >= math.pi:
+        x -= 2*math.pi
+    if x < -math.pi:
+        x += 2*math.pi
+    return x
+
 def radar_cb(event):
     ampl = []
+    phase = []
     real = []
     imag = []
     for x in event.message.cir:
         ampl.append((x.real**2+x.imag**2)**0.5)
+        phase.append(math.atan2(x.real, x.imag))
         real.append(x.real)
         imag.append(x.imag)
 
+    plt.subplot(211)
     plt.cla()
-    plt.plot(dist, ampl)
-    plt.plot(dist, real)
-    plt.plot(dist, imag)
+    plt.plot(dist, ampl, alpha=0.3)
+    plt.subplot(212)
+    plt.cla()
+    plt.plot(dist, [wrap_pi(x-phase[1]) for x in phase], alpha=0.3)
     plt.pause(0.0001)
 
 
