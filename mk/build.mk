cf19ae562d9e8358b395d86ce0751c99cfc74741
Jonathan Challinger <mr.challinger@gmail.com>
2025-09-23 12:49:08 -0400

    build: add UDEFS_EXTRA to UDEFS

--- mk/build.mk
+++ mk/build.mk
@@ -242,7 +242,7 @@ CWARN = -Wall -Wextra -Wundef -Wstrict-prototypes
 CPPWARN = -Wall -Wextra -Wundef
 
 # List all user C define here, like -D_DEBUG=1
-UDEFS += -DGIT_HASH=0x$(shell git rev-parse --short=8 HEAD) $(MODULES_ENABLED_DEFS) -DCORTEX_ENABLE_WFI_IDLE=FALSE
+UDEFS += -DGIT_HASH=0x$(shell git rev-parse --short=8 HEAD) $(MODULES_ENABLED_DEFS) -DCORTEX_ENABLE_WFI_IDLE=FALSE $(UDEFS_EXTRA)
 
 # Define ASM defines here
 UADEFS =
